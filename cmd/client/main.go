e6eb9690daf85c55bb4743dc1c2d5b584b37da9b
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-02-12 16:21:26 -0600

    Added DeclareAndBind function to pubsub.go

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	const connString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+	fmt.Println("Peril client connected to RabbitMQ")
+
+	// Prompt the user for a username
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("no username given")
+	}
+
+	queueName := routing.PauseKey + "." + username
+	fmt.Printf("the queue name is: %s\n", queueName)
+	ch, queue, err := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, queueName, routing.PauseKey, 1) // 1 for "transient" in the last arg
+	if err != nil {
+		log.Fatalf("Failed to declare and bind queue: %v", err)
+	}
+	
+	fmt.Println(ch, queue)
+	select {}
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -19,3 +19,38 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 		Body: jsonData,
 	})
 }
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType int, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("error creating channel: %v", err)
+	}
+
+	durable := true
+	autoDelete := false
+	exclusive := false
+
+	if simpleQueueType == 1 {
+		durable = false
+		autoDelete = true
+		exclusive = true
+	}
+
+	queue, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("error creating queue: %v", err)
+	}
+
+	err = ch.QueueBind(queueName, key, exchange, false, make(amqp.Table))
+	if err != nil {
+		return nil, amqp.Queue{}, fmt.Errorf("error binding queue: %v", err)
+	}
+
+	return ch, queue, nil
+}
