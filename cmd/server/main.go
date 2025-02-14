1817e4cb62164cf7f77e3b3533671de180053a95
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-02-14 11:31:18 -0600

    Implemented GetInput function in server functionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -12,6 +13,7 @@ import (
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	gamelogic.PrintServerHelp()
 	const connString = "amqp://guest:guest@localhost:5672/"
 
 	conn, err := amqp.Dial(connString)
@@ -29,17 +31,51 @@ func main() {
 	}
 	defer publishCh.Close()
 
-	err = pubsub.PublishJSON(
-		publishCh, 
-		routing.ExchangePerilDirect, 
-		routing.PauseKey, 
-		routing.PlayingState{
-			IsPaused: true,
-		},
-	)
-	if err != nil {
-		log.Fatalf("could not publish time: %v", err)
+	// Create loop
+
+	for {
+		slice := gamelogic.GetInput()
+		if len(slice) == 0 {
+			continue
+		}
+
+		if slice[0] == "pause" {
+			fmt.Println("Sending a pause message...")
+			err = pubsub.PublishJSON(
+				publishCh, 
+				routing.ExchangePerilDirect, 
+				routing.PauseKey, 
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Fatalf("could not publish message: %v", err)
+			}
+		
+			fmt.Println("Pause message sent!")
+		} else if slice[0] == "resume" {
+			fmt.Println("Sending a resume message...")
+			err = pubsub.PublishJSON(
+				publishCh, 
+				routing.ExchangePerilDirect, 
+				routing.PauseKey, 
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Fatalf("could not publish message: %v", err)
+			}
+		
+			fmt.Println("Resume message sent!")
+		} else if slice[0] == "quit" {
+			fmt.Println("Exiting...")
+			break
+		} else {
+			fmt.Println("Invalid command")
+		}
 	}
 
-	fmt.Println("Pause message sent!")
+	// End of loop creation
 }
