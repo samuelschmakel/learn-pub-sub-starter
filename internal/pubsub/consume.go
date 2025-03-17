6e9528a55cb7d1756c1f264a4c553a8d4ca2e6d7
SamSchmakel28 <samuel.schmakel@gmail.com>
2025-03-17 12:50:01 -0500

    Defined prefetch count of consumer to be 10 messages

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -95,6 +95,11 @@ func subscribe[T any](
 		return fmt.Errorf("could not declare and bind queue: %v", err)
 	}
 
+	err = ch.Qos(10, 0, false)
+	if err != nil {
+		return fmt.Errorf("could not define prefetch count: %v", err)
+	}
+
 	msgs, err := ch.Consume(
 		queue.Name,
 		"",
